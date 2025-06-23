e58f70bbd6fdf616e6cde3f68f0b95757998c769
Eugene <eugeneugene123@gmail.com>
2025-06-23 14:32:35 +0700

    InlineKeyboard added

---
+++ BooklyBarbershopBot/src/main/java/com/example/BooklyBarbershopBot/inlineButtons/InlineKeyboard.java
@@ -0,0 +1,36 @@
+package com.example.BooklyBarbershopBot.inlineButtons;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class InlineKeyboard {
+
+    public InlineKeyboardMarkup createMenuInlineKeyboard(String slug) {
+        List<List<InlineKeyboardButton>>  rows = new ArrayList<>();
+
+        rows.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("📅 Записаться")
+                        .callbackData("book_" + slug)
+                        .build(),
+                InlineKeyboardButton.builder()
+                        .text("🧾 О нас")
+                        .callbackData("about_" + slug)
+                        .build()
+        ));
+
+        rows.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("💬 Оставить отзыв")
+                        .callbackData("feedback_" + slug)
+                        .build()
+        ));
+
+        return InlineKeyboardMarkup.builder().keyboard(rows).build();
+    }
+}
